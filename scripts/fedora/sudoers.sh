58144bbb6899479ec4f48c4f31354dff372e6e62
Bao Nguyen <bao@fastly.com>
2016-07-28 13:47:20 -0700

    Building Fedora 10 box.
    This is needed to support compatible OS as Arista EOS 4.17.

--- scripts/fedora/sudoers.sh
+++ scripts/fedora/sudoers.sh
@@ -2,3 +2,4 @@
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
+sed -i -e 's/Defaults\s.*requiretty//g' /etc/sudoers
